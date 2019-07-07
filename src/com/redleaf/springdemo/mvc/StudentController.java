c85e8e05b046ef622b6c29350b035f0173510713
bagcheap <spare_brain@yahoo.com>
2019-07-07 07:32:05 -0400

    Added support for properties file for country dropdown

--- src/com/redleaf/springdemo/mvc/StudentController.java
+++ src/com/redleaf/springdemo/mvc/StudentController.java
@@ -1,13 +1,20 @@
 package com.redleaf.springdemo.mvc;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.sun.javafx.collections.MappingChange.Map;
+
 @Controller
 @RequestMapping("/student")
 public class StudentController {
+	
+
+	@Value("#{countryOptions}") 
+	private Map<String, String> countryOptions;
 	
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
